e5687b7e69b8085f06052564f948745233dc3acb
René Winkler <rene.winkler@xovis.com>
2017-11-11 17:35:37 +0100

    initial commit

---
+++ src/main/java/ch/renewinkler/springbootcamunda/SpringbootCamundaApplication.java
@@ -0,0 +1,26 @@
+package ch.renewinkler.springbootcamunda;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+@EnableProcessApplication
+public class SpringbootCamundaApplication {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("loanApproval");
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCamundaApplication.class, args);
+	}
+}
